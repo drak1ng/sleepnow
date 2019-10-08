22d0438bc55f9a953678300720666e3b4e978850
Rodrigo <rodrigo.misquini@gmail.com>
2019-10-08 16:24:34 -0300

    Initial commit

---
+++ Sleep Now/platforms/browser/www/js/my-app.js
@@ -0,0 +1,41 @@
+// Initialize app
+var app = new Framework7();
+
+// If we need to use custom DOM library, let's save it to $$ variable:
+var $$ = Dom7;
+
+// Add view
+var mainView = app.addView('.view-main', {
+    // Because we want to use dynamic navbar, we need to enable it for this view:
+    dynamicNavbar: true
+});
+
+// Handle Cordova Device Ready Event
+$$(document).on('deviceready', function() {
+    var id_usuarios = window.localStorage.getItem('id_usuarios');
+
+    console.log("Device is ready!");
+    console.log("Id user: "+id_usuarios);
+
+    if(id_usuarios=="" || id_usuarios==null){
+        mainView.router.loadPage("login.html");
+    }
+});
+
+
+// Now we need to run the code that will be executed only for About page.
+
+// Option 1. Using page callback for page (for "about" page in this case) (recommended way):
+app.onPageInit('about', function (page) {
+    // Do something here for "about" page
+
+})
+
+// Option 2. Using one 'pageInit' event handler for all pages:
+$$(document).on('pageInit', function (e) {
+    // Get page data from event data
+    var page = e.detail.page;
+
+    console.log(page.name);
+
+})
