3545ff84db4de04b0b3b5743b4a4db450ade11ae
Rodrigo <rodrigo.misquini@gmail.com>
2019-10-10 17:21:06 -0300

    beta

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,12 +14,21 @@ var mainView = app.addView('.view-main', {
 $$(document).on('deviceready', function() {
     var id_usuarios = window.localStorage.getItem('id_usuarios');
 
+    $$('#bt-lateral-sair').on('click', function (e) {
+        window.localStorage.setItem('id_usuarios','');
+        app.closePanel();
+        mainView.router.loadPage("login.html");
+    });
+
     console.log("Device is ready!");
     console.log("Id user: "+id_usuarios);
 
     if(id_usuarios=="" || id_usuarios==null){
         mainView.router.loadPage("login.html");
     }
+
+
+    
 });
 
 
@@ -37,5 +46,72 @@ $$(document).on('pageInit', function (e) {
     var page = e.detail.page;
 
     console.log(page.name);
+
+    // Script Tela - Login Manual
+    if(page.name=="login"){
+        $$('.login-bt-google').on('click', function (e) {
+            window.localStorage.setItem('id_usuarios','1');
+            mainView.router.loadPage("index.html");
+        });
+        $$('.login-bt-facebook').on('click', function (e) {
+            window.localStorage.setItem('id_usuarios','1');
+            mainView.router.loadPage("index.html");
+        });
+    }
+
+    // Script Tela - Login Manual
+    if(page.name=="login-cadastro"){
+        $$('#bt-login-acessar').on('click', function (e) {
+            var email = $$('#login-email').val();
+            var senha = $$('#login-senha').val();
+            if(email=="" || senha==""){
+                app.alert("Os campos E-mail e Senha devem ser preenchidos!","Aviso");
+                return false;
+            }
+            window.localStorage.setItem('id_usuarios','1');
+            mainView.router.loadPage("index.html");
+        });
+    }
+
+    // Script Tela - Login Cadastro
+    if(page.name=="login-cadastro-ficha"){
+        $$('#login-cadastro-ficha-bt-continuar').on('click', function (e) {
+            var nome = $$('#login-cadastro-ficha-nome').val();
+            var celular = $$('#login-cadastro-ficha-celular').val();
+            var email = $$('#login-cadastro-ficha-email').val();
+            var senha = $$('#login-cadastro-ficha-senha').val();
+            if(nome==""){
+                app.alert("O campo NOME deve ser preenchido!","Aviso");
+                return false;
+            }
+            if(celular==""){
+                app.alert("O campo CELULAR deve ser preenchido!","Aviso");
+                return false;
+            }
+            if(email==""){
+                app.alert("O campo E-MAIL deve ser preenchido!","Aviso");
+                return false;
+            }
+            if(senha==""){
+                app.alert("O campo SENHA deve ser preenchido!","Aviso");
+                return false;
+            }
+            window.localStorage.setItem('id_usuarios','1');
+            mainView.router.loadPage("index.html");
+        });
+    }
+
+    // Script Tela - Esqueci minha senha
+    if(page.name=="login-cadastro-esqueci"){
+        $$('#bt-recuperar-senha').on('click', function (e) {
+            var email = $$('#recuperar-senha-email').val();
+            if(email==""){
+                app.alert("Você deve informar um e-mail válido!","Aviso");
+                return false;
+            }
+            window.localStorage.setItem('id_usuarios','1');
+            mainView.router.loadPage("login-esqueci-senha-obrigado.html");
+        });
+    }
 
 })
