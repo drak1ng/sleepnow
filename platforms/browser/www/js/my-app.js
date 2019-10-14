a89ef22485b43ea913500084dca4955b63ade461
Rodrigo <rodrigo.misquini@gmail.com>
2019-10-14 18:38:46 -0300

    Beta

--- platforms/browser/www/js/my-app.js
+++ platforms/browser/www/js/my-app.js
@@ -65,6 +65,21 @@ $$(document).on('pageInit', function (e) {
             window.localStorage.setItem('id_usuarios','1');
             mainView.router.loadPage("index.html");
         });
+
+        function onSignIn(googleUser) {
+            // Useful data for your client-side scripts:
+            var profile = googleUser.getBasicProfile();
+            console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+            console.log('Full Name: ' + profile.getName());
+            console.log('Given Name: ' + profile.getGivenName());
+            console.log('Family Name: ' + profile.getFamilyName());
+            console.log("Image URL: " + profile.getImageUrl());
+            console.log("Email: " + profile.getEmail());
+    
+            // The ID token you need to pass to your backend:
+            var id_token = googleUser.getAuthResponse().id_token;
+            console.log("ID Token: " + id_token);
+        }
     }
 
     // Script Tela - Login Manual
