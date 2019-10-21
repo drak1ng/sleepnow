5f35d55d0158e77cd2a1ba5849a6186525f0ef01
Drak1ng <>
2019-10-21 18:31:02 -0300

    beta

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -144,7 +144,7 @@ $$(document).on('page:init', function (e) {
             window.plugins.googleplus.login(
             {
                 'scopes': 'email profile', // optional, space-separated list of scopes, If not included or empty, defaults to `profile` and `email`.
-                'webClientId': 'client id of the web app/server side', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
+                'webClientId': '559664173319-u04gf9i8p45e4bqnrsc57q3jfgppj978.apps.googleusercontent.com', // optional clientId of your Web application from Credentials settings of your project - On Android, this MUST be included to get an idToken. On iOS, it is not required.
                 'offline': true // optional, but requires the webClientId - if set to true the plugin will also return a serverAuthCode, which can be used to grant offline access to a non-Google server
             },
             function (obj) {
